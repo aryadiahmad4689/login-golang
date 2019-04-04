667e820179c45a78dba31961c52dc66ff53b90ad
aryadiahmad4689 <sahrulafa45@gmail.com>
2019-04-05 02:56:20 +0800

    perbaikan bug

--- controllers/loginController.go
+++ controllers/loginController.go
@@ -15,11 +15,13 @@ func IndexLogin(c echo.Context) error {
 	return c.Render(http.StatusOK, "login", map[string]interface{}{})
 }
 
+// cek hash password
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
 
+// func Login
 func LoginSave(c echo.Context) error {
 	db := db.DbManager()
 	user := []models.User{}
@@ -27,17 +29,12 @@ func LoginSave(c echo.Context) error {
 	usernames := c.FormValue("username")
 	db.Table("users").Select("*").Scan(&user)
 	for _, v := range user {
-
 		username := fmt.Sprintf("%s", v.Username)
 		pass := fmt.Sprintf("%s", v.Password)
-
 		match := CheckPasswordHash(password, pass)
 		if username == usernames && match == true {
 			return c.JSON(http.StatusOK, user)
-		} else {
-			return c.JSON(http.StatusOK, "Password Dan Email Anda Salah")
 		}
-
 	}
-	return nil
+	return c.JSON(http.StatusOK, "Password Dan Username Salah")
 }
